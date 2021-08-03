c9311085ee62b510993bbb9ea787b8c00c6dc48f
SamArsanios <samson.kibrom@hotmail.com>
2021-08-03 23:58:23 +0300

    Add cabins components

---
+++ src/components/container/Cabins.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchCabins } from '../../redux/actions/cabinActions'
+
+class Cabins extends Component {
+  componentDidMount() {
+    const { fetchCabins } = this.props;
+    fetchCabins();
+}
+
+  render() {
+    const { cabins, errors, loading } = this.props;
+
+    const cabinsLoad = cabins.length ? (
+      <div className="container-xl">
+        <div className="cabin-container my-4 py-3">
+          {cabins
+            && cabins.map(cabin => (
+              <CabinLists cabin={cabin} key={cabin.id} errors={errors} />
+            ))}
+        </div>
+      </div>
+    ) : (
+      <div className="loading">
+        {loading && <Loading />}
+        {errors && <Errors />}
+      </div>
+    );
+
+    return cabinsLoad;
+  }
+}
+
+Cabins.propTypes = {
+  errors: PropTypes.any,
+  cabins: PropTypes.any,
+  loading: PropTypes.any,
+  fetchCabins: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  cabins: state.data.cabins,
+  loading: state.data.loading,
+  errors: state.error.err,
+  cabin: state.data.cabin,
+})
+
+export default connect(mapStateToProps, { fetchCabins })(Cabins);
