2836faeedaf62d28b28638bb3049076502fdd0dd
SamArsanios <samson.kibrom@hotmail.com>
2021-08-11 11:56:43 +0300

    Add user reducer test

---
+++ src/__test__/reducers/userreducer.test.js
@@ -0,0 +1,11 @@
+import userReducer, { initialState } from '../../redux/reducers/userReducer';
+
+test('initialState to be as expected', () => {
+  expect(initialState).toEqual({
+    loading: true,
+    username: '',
+    loggedIn: false,
+    currentUser: {},
+    userFavourites: [],
+  });
+});
