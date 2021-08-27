f20285814b574a2e9ddb5991dbdd3299752aae93
SamArsanios <samson.kibrom@hotmail.com>
2021-08-27 12:04:43 +0300

    Clean code

--- src/redux/actions/favActions.js
+++ src/redux/actions/favActions.js
@@ -1,129 +1,13 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable camelcase */
-// import Axios from 'axios';
 import baseURL from './baseURL';
 import queryBuilder from './actionHelper';
-
-// const addToFavourites = (data, user) => (dispatch) => {
-//   const token = localStorage.getItem('jwt');
-//   const authAxios = Axios.create({
-//     baseURL: `${baseURL}`,
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       'Content-Type': 'application/json',
-//     },
-//   });
-
-//   const cabinData = {
-//     favourite: data,
-//   };
-//   const payload = {
-//     message: 'Cabin was successfully added to Favourites!',
-//     type: 'add_fav',
-//   };
-
-//   authAxios
-//     .post('/api/v1/favourites.json', cabinData)
-//     .then((res) => {
-//       dispatch({
-//         type: 'ADD_FAVOURITE',
-//         payload: res.data.cabin_id,
-//       });
-
-//       dispatch({
-//         type: 'FETCH_CABIN',
-//         payload: res.data.cabin_id,
-//       });
-
-//       dispatch({
-//         type: 'FAVOURITE_CABIN',
-//       });
-
-//       dispatch({
-//         type: 'SUCCESS_MESSAGE',
-//         payload,
-//       });
-
-//       dispatch({
-//         type: 'FETCH_USER',
-//         payload: user,
-//       });
-//     })
-//     .catch((err) => dispatch({
-//       type: 'CREATE_ERROR',
-//       payload: err,
-//     }));
-// };
 
 const addToFavourites = (data, user) => queryBuilder().postFavourites('/api/v1/favourites.json', data, user);
 
 const removeFromFavourites = (cabin_id, user) => queryBuilder().removeFavourites(`${baseURL}/api/v1/favourites/${cabin_id}.json`, cabin_id, user);
 
 const uploadImage = (image) => queryBuilder().postImage(image);
-// const removeFromFavourites = (cabin_id, user) => (dispatch) => {
-//   const token = localStorage.getItem('jwt');
-//   const authAxios = Axios.create({
-//     baseURL: `${baseURL}`,
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       'Content-Type': 'application/json',
-//     },
-//   });
-
-//   const payload = {
-//     message: 'Cabin was successfully removed from Favourites!',
-//     type: 'remove_fav',
-//   };
-//   authAxios
-//     .delete(`/api/v1/favourites/${cabin_id}.json`)
-//     .then(() => {
-//       dispatch({
-//         type: 'REMOVE_FAVOURITE',
-//         payload: cabin_id,
-//       });
-
-//       dispatch({
-//         type: 'NOT_FAVOURITE_CABIN',
-//       });
-
-//       dispatch({
-//         type: 'SUCCESS_MESSAGE',
-//         payload,
-//       });
-
-//       dispatch({
-//         type: 'FETCH_USER',
-//         payload: user,
-//       });
-//     })
-//     .catch((err) => dispatch({
-//       type: 'CREATE_ERROR',
-//       payload: err,
-//     }));
-// };
-
-// const uploadImage = (image) => (dispatch) => {
-//   const token = localStorage.getItem('jwt');
-//   const userAxios = Axios.create({
-//     baseURL: `${baseURL}`,
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-
-//   userAxios
-//     .post('/api/v1/image_uploaders.json', image)
-//     .then((res) => {
-//       dispatch({
-//         type: 'ADD_IMAGE',
-//         payload: res.data,
-//       });
-//     })
-//     .catch((err) => dispatch({
-//       type: 'CREATE_ERROR',
-//       payload: err,
-//     }));
-// };
 
 const clearImage = () => (dispatch) => {
   dispatch({
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,3 @@
-// import Axios from 'axios';
 import baseURL from './baseURL';
 import queryBuilder from './actionHelper';
 
@@ -9,94 +8,6 @@ const authorizeUser = (data) => queryBuilder().authorizeUser(data);
 const fetchUser = () => queryBuilder().get('/api/v1/users/', 'FETCH_USER');
 
 const fetchUserFavourites = () => queryBuilder().get('/api/v1/favourites', 'ADD_FAVOURITE');
-
-// Create User
-// const createUser = (data) => (dispatch) => {
-//   const url = `${baseURL}/api/v1/users`;
-//   const userData = {
-//     user: data,
-//   };
-
-//   Axios.post(url, userData)
-//     .then((res) => {
-//       dispatch({
-//         type: 'CREATE_USER',
-//         payload: res.data,
-//       });
-//     })
-//     .catch((err) => dispatch({
-//       type: 'CREATE_ERROR',
-//       payload: err,
-//     }));
-// };
-
-// Authorize User
-// const authorizeUser = (data) => (dispatch) => {
-//   const url = `${baseURL}/user_token`;
-//   const userData = {
-//     auth: data,
-//   };
-
-//   Axios.post(url, userData)
-//     .then((res) => {
-//       dispatch({
-//         type: 'AUTHORIZE_USER',
-//         payload: res.data,
-//         username: userData.auth.username,
-//       });
-//     })
-//     .catch((err) => dispatch({
-//       type: 'CREATE_ERROR',
-//       payload: err,
-//     }));
-// };
-
-// Fetch user
-// const fetchUser = () => (dispatch) => {
-//   const token = localStorage.getItem('jwt');
-
-//   const userAxios = Axios.create({
-//     baseURL: `${baseURL}`,
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-
-//   userAxios.get('/api/v1/users/')
-//     .then((res) => dispatch({
-//       type: 'FETCH_USER',
-//       payload: res.data,
-//     }))
-//     .catch((err) => {
-//       dispatch({
-//         type: 'CREATE_ERROR',
-//         payload: err,
-//       });
-//     });
-// };
-
-// Fetch User's Favourites
-// const fetchUserFavourites = () => (dispatch) => {
-//   const token = localStorage.getItem('jwt');
-//   const favouriteAxios = Axios.create({
-//     baseURL: `${baseURL}`,
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-
-//   favouriteAxios.get('/api/v1/favourites')
-//     .then((res) => {
-//       dispatch({
-//         type: 'ADD_FAVOURITE',
-//         payload: res.data,
-//       });
-//     })
-//     .catch((err) => dispatch({
-//       type: 'CREATE_ERROR',
-//       payload: err,
-//     }));
-// };
 
 // Logout Current User
 const logOutCurrentUser = () => (dispatch) => {
